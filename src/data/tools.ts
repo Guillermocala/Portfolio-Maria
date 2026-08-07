3369b4e9986cc142ef18d7d0f498f5be1497cf80
Guillermo Cala <guillermo.cala7@gmail.com>
2026-08-06 20:42:27 -0500

    Agregando imports de herramientas

--- src/data/tools.ts
+++ src/data/tools.ts
@@ -1,9 +1,14 @@
 import type { Tool } from './types'
+import illustratorLogo from "@/assets/tools/adobe-illustrator-logo.svg";
+import photoshopLogo from "@/assets/tools/adobe-photoshop-logo.svg";
+import canvaLogo from "@/assets/tools/canva-logo.svg";
+import capcutLogo from "@/assets/tools/capcut-logo.svg";
+import officeLogo from "@/assets/tools/office-logo.svg";
 
 export const tools: Tool[] = [
-  { name: 'Illustrator', src: '/src/assets/tools/adobe-illustrator-logo.svg' },
-  { name: 'Photoshop', src: '/src/assets/tools/adobe-photoshop-logo.svg' },
-  { name: 'Canva', src: '/src/assets/tools/canva-logo.svg' },
-  { name: 'CapCut', src: '/src/assets/tools/capcut-logo.svg' },
-  { name: 'Office', src: '/src/assets/tools/office-logo.svg' },
+  { name: 'Illustrator', src: illustratorLogo },
+  { name: 'Photoshop', src: photoshopLogo },
+  { name: 'Canva', src: canvaLogo },
+  { name: 'CapCut', src: capcutLogo },
+  { name: 'Office', src: officeLogo },
 ]
